ad3ee29c10efd2e25df03221e7c311076bda517c
Eduardo Nhimi Chicaia <141135814+EduardoChicaia@users.noreply.github.com>
2025-08-01 15:25:59 +0100

    Exercício de aplicação array, function, object

---
+++ pratica/js/javascript.js
@@ -0,0 +1,28 @@
+function meuEscopo() {
+    const form = document.querySelector('.form')
+    const pessoa = []
+    const resultado = document.querySelector('.resultado')
+
+    function pararEvento (evento) {
+
+        evento.preventDefault()
+
+        const nome = form.querySelector('.nome')
+        const sobre = form.querySelector('.sobre')
+        const peso = form.querySelector('.peso')
+        const altura = form.querySelector('.altura')
+
+        pessoa.push({
+            nome: nome.value,
+            sobre: sobre.value,
+            peso: peso.value,
+            altura: altura.value
+        })
+        
+        resultado.innerHTML += `<p>${nome.value} ${sobre.value} ${peso.value} ${altura.value}</p>`
+        console.log(pessoa)
+    }
+
+    form.addEventListener('submit', pararEvento);
+}
+meuEscopo()
